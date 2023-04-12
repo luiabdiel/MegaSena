81bc339f6a8942d1656215396d001106cd4079a1
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-04-12 15:26:52 -0300

    Loops with while

--- src/MegaSena.java
+++ src/MegaSena.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class MegaSena {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        Random generate = new Random();
+
+        int i = 0;
+        while(i < 6) {
+            int number = generate.nextInt(60);
+
+            System.out.println(number);
+            i++;
+        }
     }
 }
